5c19ff545531f5777e299d0a2b1d2de548da53b5
James Holland <jamoiholland@gmail.com>
2019-01-11 17:05:48 +0000

    Update metasploit-v5-initialise-db.sh

--- metasploit-v5-initialise-db.sh
+++ metasploit-v5-initialise-db.sh
@@ -14,6 +14,11 @@ spawn msfconsole
 match_max 100000
 expect -exact "Would you like to use and setup a new database (recommended)? "
 send -- "yes\r"
-expect ">"
-send -- "exit\r"
+expect "]:"
+send -- "\r"
+expect -exact "Add data service connection to local msfconsole and persist as default?: "
+send -- "yes\r"
+expect -exact "Data service connection name?\[local-https-data-service\]: "
+send -- "\r"
+
 expect eof
